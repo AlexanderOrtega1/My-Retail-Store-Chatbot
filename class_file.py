73b29129afb1e6c076ed001f441b23ac287f36e8
AlexanderOrtega1 <154935339+AlexanderOrtega1@users.noreply.github.com>
2024-02-23 17:43:05 +0000

    Created saving feature for each list. Created new file for any class function

---
+++ class_file.py
@@ -0,0 +1,10 @@
+
+class AccountCreation:
+  def __init__(self, email, password):
+    self.email = email
+    self.password = password
+
+  def create_account(self):
+    return {'email': self.email, 'password': self.password}
+
+
--- main.py
+++ main.py
@@ -1,53 +1,84 @@
 import os
 import time
+import class_file
 
-
-# # # variables/lists # # #
-storeinv = []
+# # # # # # # variables/lists # # # # # # #
+store_inv = []
 used_PIDs = []
 user_acc = []
 emp_acc = []
-print(emp_acc)
+# # # saving functions # # #
+def user_account_file_write():
+  f = open('accounts.txt', 'w')
+  f.write(str(user_acc))
+  f.close()
+def employee_account_file_write():
+  f = open('accounts.txt', 'w')
+  f.write(str(emp_acc))
+  f.close()
+def used_PIDs_file_write():
+  f = open('accounts.txt', 'w')
+  f.write(str(used_PIDs))
+  f.close()
+def store_inv_file_write():
+  f = open('accounts.txt', 'w')
+  f.write(str(store_inv))
+  f.close()
+
 try:
   f = open("store.inv", "r")
-  storeinv = eval(f.read())
+  store_inv = eval(f.read())
   f.close()
 except:
-  storeinv = []
-
+  store_inv = []
+  store_inv_file_write()
 try:
   f = open("used.PIDs", "r")
   used_PIDs = eval(f.read())
   f.close()
 except:
   used_PIDs = []
-
+  used_PIDs_file_write()
 try:
   f = open("user.acc", "r")
   user_acc = eval(f.read())
   f.close()
 except:
   user_acc = []
-
+  user_account_file_write()
 try:
   f = open("emp.acc", "r")
   emp_acc = eval(f.read())
   f.close()
 except:
   emp_acc = []
-print(emp_acc)
+  employee_account_file_write()
+# # # # # # # # # # # # # # # #
+  
 list_index = 0
 
 admin_code = "admin" # change this later#
 textSeperator = '==================================================================='
 # # #
-# # # functions # # #
+# # # # # # # functions # # # # # # #
 def timeClear(t):
   time.sleep(t)
   os.system("clear")
 
+def create_account(email, password):
+  new_account = AccountCreation(email, password)
+  if user_acc == []:
+    user_acc.append(new_account.create_account())
+    user_account_file_write_file_write()
+  else:
+    if any(account['email'] == email for account in user_acc):
+      print("Email already in use")
+    else:
+      user_acc.append(new_account.create_account())
+      user_account_file_write()
 
-# # # M A I N # # #
+
+# # # # # # # M A I N # # # # # # #
 # login stuffs #
 print(textSeperator)
 print('Hello, User!! Welcome to The Retail Store chatbot!!')
@@ -58,36 +89,6 @@ print(textSeperator)
 os.system('clear')
 
 if returning == '1' or returning == 'new':
-  while True:
-    print(textSeperator)
-    print('Please create a username and password to continue.')
-    print()
-    username = input('Username: ')
-    password = input('Password: ')
-    print(textSeperator)
-    for i in user_acc:
-      if i in user_acc:
-        print('This username alreay exists. Please try again.')
-        timeClear(2)
-        continue
-      else:
-        while True:
-          print('Please confirm your username and password.')
-          print()
-          username_confirm = input('Username: ')
-          password_confirm = input('Password: ')
-          print(textSeperator)
-          if username_confirm == username and password_confirm == password:
-            os.system('clear')
-            print(textSeperator)
-            print(f'Welcome to the Retail Store chatbot, {username}!')
-            user_acc.append(f'{username}: {password}')
-            print(textSeperator)
-            print(user_acc)
-            break
-          else:
-            print('Your username or password is incorrect. Please try again.')
-            print(textSeperator)
-            continue
+  
       
 #elif returning == '2' or returning == 'returning':
